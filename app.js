fe772a9d7377b9b8b3fe443b6ae9c7f0d42292f7
Cristiano Ferreira <96497604+bitcris@users.noreply.github.com>
2023-07-11 18:53:28 +0000

    rue

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const axios = require('axios');
 const { engine } = require('express-handlebars');
-const session = require('express-session')
 
 const app = express();
 
@@ -9,14 +9,56 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home');
-    console.log(req.rawHeaders[1]);
+async function getIPDetails(ip) {
+  try {
+    const response = await axios.get(`https://freeipapi.com/api/json/${ip}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+app.get('/', async (req, res) => {
+  const userAgent = req.headers['user-agent'];
+  const cleanedText = userAgent.replace('user-agent = ', '');
+  const items = cleanedText.split(' ');
+  const hasMozilla = items.some(item => item.includes('Mozilla'));
+
+  const clientIP = '186.249.86.64'; //req.ip
+  const ip = clientIP.split(':').pop();
+  
+
+
+  const parseUserAgent = require('./parser.js');
+  const text = cleanedText;
+  const result = parseUserAgent(text);
+  console.log(result);
+  
+  
+
+
+  try {
+    const dados = await getIPDetails(ip);
+    console.log(dados);
+
+    if (hasMozilla) {
+      res.render('home', { ip, dados, result });
+      
+    } else {
+      const acceptHeader = req.headers['accept'];
+      const isBrowserRequest = acceptHeader && acceptHeader.includes('text/html');
+
+      if (isBrowserRequest) {
+        res.json(dados);
+      } else {
+        res.send(dados);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.render('error'); // Renderiza uma página de erro, se necessário
+  }
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
-    console.log(req.rawHeaders[1]);
-});
-
-app.listen(3000);
+app.listen(80);
